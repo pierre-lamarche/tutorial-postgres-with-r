7ec3c19130b51a6b9fe6da2f79b728e9495e75c5
pierre-lamarche <lamarche.p@gmail.com>
2020-06-01 22:05:48 +0000

    Export de la fonction de lancement

--- R/fonctions.R
+++ R/fonctions.R
@@ -1,3 +1,10 @@
+#' Lance le tutoriel
+#'
+#' @param file le file Rmd du tutoriel
+#' @param port le port sur lequel on envoie la page web
+#' @param host l'url sur laquelle on publie
+#'
+#' @export
 launch_tutorial <- function(file, port = 8181, host = 'localhost') {
   rmarkdown::run(file = file,
                  shiny_args = list(host = host, port = port, launch.browser = FALSE))
